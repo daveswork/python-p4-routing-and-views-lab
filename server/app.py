8fee600c6ca81aaae864210f496c994bdb3ca923
Dave J <29786887+daveswork@users.noreply.github.com>
2024-09-13 14:24:35 -0400

    All local tests pass.

--- server/app.py
+++ server/app.py
@@ -4,5 +4,36 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return '<h1>Python Operations with Flask Routing and Views</h1>'
+
+@app.route('/print/<string:example>')
+def print_string(example):
+    print(example)
+    return f'{example}'
+
+@app.route('/count/<int:some_number>')
+def count(some_number):
+    num_range = ''
+    for num in range(some_number):
+        num_range += f'{num}\n'
+    return num_range
+
+@app.route('/math/<num1>/<operation>/<num2>')
+def math(num1, operation, num2):
+    total = 0
+    if operation == "+":
+        total = int(num1) + int(num2)
+    elif operation == "-":
+        total = int(num1) - int(num2)
+    elif operation == "*":
+        total = int(num1) * int(num2)
+    elif operation == "div":
+        total = int(num1) / int(num2)
+    elif operation == "%":
+        total = int(num1) % int(num2)
+    return str(total)
+
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
